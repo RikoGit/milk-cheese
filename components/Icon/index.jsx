9d28dd8a1700dc75ff4eb67fc54d8a379b88d83b
RikoGit <r.git@lenta.ru>
2021-02-16 19:56:56 +0300

    Add images for cards

--- components/Icon/index.jsx
+++ components/Icon/index.jsx
@@ -3,21 +3,16 @@ import cn from "classnames";
 
 import styles from "./styles.scss";
 
-const Icon = ({ icon = "add", type, active, size = 20 }) => {
-  const activeStyles = active ? styles.root_active : "";
-  const typeStyles = type ? styles[`root_type_${type}`] : "";
-
-  return (
-    <div
-      className={cn(
-        styles.root,
-        styles[`root_icon_${icon}`],
-        typeStyles,
-        activeStyles
-      )}
-      style={{ width: size, height: size }}
-    />
-  );
-};
+const Icon = ({ icon = "add", type, active, size = 20 }) => (
+  <div
+    className={cn(
+      styles.root,
+      styles[`root_icon_${icon}`],
+      styles[`root_type_${type}`],
+      active && styles.root_active
+    )}
+    style={{ width: size, height: size }}
+  />
+);
 
 export default Icon;
